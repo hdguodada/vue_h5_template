7dc4f77d26b3e40a9859799ae78c507761f2f982
郭大大 <261731963@qq.com>
2022-08-05 14:51:38 +0800

    fst

---
+++ postcss.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    'postcss-pxtorem': {
+      rootValue({ file }) {
+        return file.indexOf('vant') !== -1 ? 37.5 : 75;
+      },
+      propList: ['*'],
+    },
+  },
+}
